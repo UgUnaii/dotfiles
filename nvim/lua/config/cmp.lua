d910071be5d04813e445232d13de699fafdfc5d9
Unaii <roes.unai@gmail.com>
2024-11-03 18:15:18 +0100

    added cmp and luasnip

--- nvim/lua/config/cmp.lua
+++ nvim/lua/config/cmp.lua
@@ -1,34 +1,48 @@
 return {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",      -- Autocompletado LSP
-        "hrsh7th/cmp-buffer",        -- Autocompletado de buffers
-        "hrsh7th/cmp-path",          -- Autocompletado de rutas de archivos
-        "hrsh7th/cmp-cmdline",       -- Autocompletado de la línea de comandos
-        "saadparwaiz1/cmp_luasnip",  -- Autocompletado para LuaSnip
-        "L3MON4D3/LuaSnip",          -- Motor de snippets
-    },
-    config = function()
-        local cmp = require("cmp")
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "saadparwaiz1/cmp_luasnip",
+            "L3MON4D3/LuaSnip",
+            "ray-x/lsp_signature.nvim",  -- Para la firma de parámetros
+        },
+        config = function()
+            local cmp = require("cmp")
+            local lsp_signature = require("lsp_signature")
 
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- Usar LuaSnip para expandir snippets
-                end,
-            },
-            mapping = {
-                ['<C-Space>'] = cmp.mapping.complete(), -- Trigger completion
-                ['<Down>'] = cmp.mapping.select_next_item(), -- Select next item
-                ['<Up>'] = cmp.mapping.select_prev_item(), -- Select previous item
-                ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Confirm selection
-            },
-            sources = {
-                { name = 'nvim_lsp' }, -- Autocompletado LSP
-                { name = 'buffer' },   -- Autocompletado de buffer
-                { name = 'path' },     -- Autocompletado de ruta
-                { name = 'luasnip' },  -- Snippets
-            },
-        })
-    end
-}
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                mapping = {
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<Down>'] = cmp.mapping.select_next_item(),
+                    ['<Up>'] = cmp.mapping.select_prev_item(),
+                    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+                },
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'buffer' },
+                    { name = 'path' },
+                    { name = 'luasnip' },
+                }),
+                preselect = cmp.PreselectMode.Item,
+            })
+
+            -- Configuración de lsp_signature para mostrar parámetros al llamar funciones
+            lsp_signature.setup({
+                bind = true,
+                floating_window = true,
+                hint_enable = true,
+                hint_prefix = "🔹 ",
+                handler_opts = {
+                    border = "rounded"
+                }
+            })
+        end
+    }
--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,56 +1,59 @@
 return {
-    {
         "neovim/nvim-lspconfig",
-	dependencies = { 
-	    "b0o/schemastore.nvim",
+        dependencies = {
+            "b0o/schemastore.nvim",
         },
         config = function()
             local lspconfig = require("lspconfig")
+            local on_attach = function(client, bufnr)
+                -- Configuración de firma de función en cada servidor LSP
+                require("lsp_signature").on_attach({
+                    bind = true,
+                    floating_window = true,
+                    hint_enable = false,
+                    handler_opts = {
+                        border = "single"
+                    },
+                }, bufnr)
+            end
 
-            -- Typescript, Javascript, TSX, JSX
+            -- Configuración de servidores LSP específicos
             lspconfig.ts_ls.setup({
                 on_attach = on_attach,
                 filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact" },
             })
 
-            -- HTML
             lspconfig.html.setup({
                 on_attach = on_attach,
                 filetypes = { "html" },
             })
 
-            -- CSS
             lspconfig.cssls.setup({
                 on_attach = on_attach,
                 filetypes = { "css", "scss", "less" },
             })
 
-            -- Java
             lspconfig.jdtls.setup({
                 on_attach = on_attach,
                 filetypes = { "java" },
-                cmd = { 'jdtls' }, 
+                cmd = { "jdtls" },
             })
 
-            -- JSON
             lspconfig.jsonls.setup({
                 on_attach = on_attach,
                 filetypes = { "json" },
                 settings = {
                     json = {
-                        schemas = require('schemastore').json.schemas(),
+                        schemas = require("schemastore").json.schemas(),
                         validate = { enable = true }
                     }
                 }
             })
 
-            -- GLSL
             lspconfig.glslls.setup({
                 on_attach = on_attach,
                 filetypes = { "glsl" },
-                cmd = { "glslangValidator" }, 
+                cmd = { "glslangValidator" },
             })
         end
     }
-}
-
